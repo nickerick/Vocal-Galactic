3d64ad815a842f6c7e6d28d2a7f1dd6701205003
Nicholas Erickson <86501156+nickerick@users.noreply.github.com>
2021-11-13 01:04:13 -0600

    Update Driving.java

--- src/Driving.java
+++ src/Driving.java
@@ -1,26 +1,28 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import javax.swing.*;
+
 public class Driving {
     private static JFrame Game = new JFrame();
     public static Panel1 play = new Panel1();
     private static Panel2 title = new Panel2();
     private static Panel3 tutorial = new Panel3();
     private static  Panel4 settings = new Panel4();
-    public static void main(String[] args) throws IOException {
-    	String str = "Not Found";
-	    BufferedWriter writer = new BufferedWriter(new FileWriter("src\\link.txt"));
-	    writer.write(str);
-	    writer.close();
+    
+	public static void main(String[] args) throws IOException {
+	String str = "Not Found";
+	BufferedWriter writer = new BufferedWriter(new FileWriter("src\\link.txt"));
+	writer.write(str);
+	writer.close();
+	    
         MusicPlayer.playMusic();
+	    
         Game.getContentPane().add(title);
         Game.pack();
         Game.setVisible(true);
         Game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Game.setResizable(false);
-
     }
 
     public static void switchToGame()
